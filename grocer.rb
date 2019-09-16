1578c95100a8f88b45d1dd1fe8fad17cba6076ec
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-09-16 05:59:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -17,7 +17,7 @@ def apply_coupons(cart, coupons)
     item = coupon[:item]
     if cart.has_key?[item]
       if cart[item][:count] >= coupon[:num] && !cart.has_key?("#{item} W/COUPON")
-        
+        cart |
 end
 
 def apply_clearance(cart)
