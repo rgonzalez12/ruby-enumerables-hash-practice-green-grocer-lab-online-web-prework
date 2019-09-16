2f8e485c823fea8748fb4ef129340f310fcedfce
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-09-16 06:03:17 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -13,6 +13,30 @@ def consolidate_cart(cart)
 end
 
 def apply_coupons(cart, coupons)
+  hash = cart
+  coupons.each do |coupon_hash|
+    # add coupon to cart
+    item = coupon_hash[:item]
+
+    if !hash[item].nil? && hash[item][:count] >= coupon_hash[:num]
+      temp = {"#{item} W/COUPON" => {
+        :price => coupon_hash[:cost],
+        :clearance => hash[item][:clearance],
+        :count => 1
+        }
+      }
+      
+      if hash["#{item} W/COUPON"].nil?
+        hash.merge!(temp)
+      else
+        hash["#{item} W/COUPON"][:count] += 1
+        #hash["#{item} W/COUPON"][:price] += coupon_hash[:cost]
+      end
+      
+      hash[item][:count] -= coupon_hash[:num]
+    end
+  end
+  hash
   
 end
 
