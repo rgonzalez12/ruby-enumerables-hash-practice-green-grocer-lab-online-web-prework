2517862d752de0271125209415aa7f5a7e12f3d5
Raymond Gonzalez <raymondalexander.gonzalez@gmail.com>
2019-09-16 05:42:31 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,15 +1,23 @@
 def consolidate_cart(cart)
-  # code here
+  new_cart = {} 
+  cart.each do |items_array| 
+    items_array.each do |item, attribute_hash| 
+      new_cart[item] ||= attribute_hash 
+      new_cart[item][:count] ? new_cart[item][:count] += 1 :   
+      new_cart[item][:count] = 1 
+  end 
+end 
+new_cart 
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  #your code here
 end
 
 def apply_clearance(cart)
-  # code here
+  #your code here
 end
 
 def checkout(cart, coupons)
-  # code here
+  #your code here
 end
